c84efc7d7ebb5f7d49de4d08f11da7b2435ed8dc
rozaki <eleftheria.rozi@gmail.com>
2016-03-01 14:57:41 +0100

    Doctrine pushing data to yoda_event table

---
+++ play.php
@@ -0,0 +1,43 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+umask(0000);
+
+$loader = require_once __DIR__.'/app/bootstrap.php.cache';
+Debug::enable();
+
+require_once __DIR__.'/app/AppKernel.php';
+
+$kernel = new AppKernel('dev', true);
+$kernel->loadClassCache();
+$request = Request::createFromGlobals();
+$kernel->boot();
+
+$container = $kernel->getContainer();
+$container->enterScope('request');
+$container->set('request', $request);
+
+// all our setup is done!!!!!!
+use Yoda\EventBundle\Entity\Event;
+
+$event = new Event();
+$event->setName('Darth\'s surprise birthday party');
+$event->setLocation('Dethstar');
+$event->setTime(new \DateTime('tomorrow noon'));
+$event->setDetails('Ha! Dearth hates surprises!');
+
+$em = $container->get('doctrine')->getManager();
+$em->persist($event);
+$em->flush();
+
+/*$templating = $container->get('templating');
+
+echo $templating->render(
+    'EventBundle:Default:index.html.twig',
+    array(
+        'name' => 'Yoda',
+        'count' => 5,
+    )
+);
+*/
